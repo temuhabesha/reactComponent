146de52af4336b0c06eb8d6fd2603886625d855d
ethiotom <temesgenwondim77@gmail.com>
2025-07-20 01:49:43 +0300

    this is the first commit of this project when we learn react component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>this is the middle class for react js</h1>
+    </>
+  )
+}
+
+
+
+
+export default App
